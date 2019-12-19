41d030e09309aceefc1e5aa463d2fb99724c9210
tinnakorn pornsontisakul <tinnakorn.pornsontisakul@scb.co.th>
2019-12-19 11:48:59 +0700

    POC_Way4: add swipe view

---
+++ Playground/Playground/NewCard/SwipeView/SwipeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SwipeViewController.swift
+//  Playground
+//
+//  Created by Tnk. on 19/12/2562 BE.
+//  Copyright © 2562 Tnk. All rights reserved.
+//
+
+import UIKit
+
+class SwipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
